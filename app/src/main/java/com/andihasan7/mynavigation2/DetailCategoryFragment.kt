bb8d8e14769d814e3f9e32d19422e96777732758
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-09 16:40:45 +0700

    mengirim data dengan bundle

--- app/src/main/java/com/andihasan7/mynavigation2/DetailCategoryFragment.kt
+++ app/src/main/java/com/andihasan7/mynavigation2/DetailCategoryFragment.kt
@@ -5,23 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import com.andihasan7.mynavigation2.databinding.FragmentCategoryBinding
+import com.andihasan7.mynavigation2.databinding.FragmentDetailCategoryBinding
 
-// mengirim data dengan bundle
 // TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-class CategoryFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailCategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailCategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var _binding: FragmentCategoryBinding? = null
+    private var _binding: FragmentDetailCategoryBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -29,21 +34,15 @@ class CategoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategoryLifestyle.setOnClickListener { view ->
-            val mBundle = Bundle()
-            mBundle.putString(EXTRA_NAME, "Lifestyle")
-            mBundle.putLong(EXTRA_STOCK, 7)
-            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
-        }
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    companion object {
-        val EXTRA_NAME = "extra_name"
-        val EXTRA_STOCK = "extra_stock"
     }
 }
